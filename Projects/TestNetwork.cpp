254b8365226698a992f59fa4724d502ac735c2da
oceancx <oceancx@gmail.com>
2017-09-03 20:57:06 +0800

    Add network code

--- Projects/TestNetwork.cpp
+++ Projects/TestNetwork.cpp
@@ -1,6 +1,182 @@
 #include "TestNetwork.h"
 #include "Logger.h"
-#pragma comment (lib,"ws2_32.lib")
+
+
+CTcpListener::CTcpListener(std::string ipAddress, int port, MessageRecievedHandler handler)
+	: m_ipAddress(ipAddress), m_port(port), MessageReceived(handler)
+{
+
+}
+
+CTcpListener::~CTcpListener()
+{
+	Cleanup();
+}
+
+// Send a message to the specified client
+void CTcpListener::Send(int clientSocket, std::string msg)
+{
+	send(clientSocket, msg.c_str(), msg.size() + 1, 0);
+}
+
+// Initialize winsock
+bool CTcpListener::Init()
+{
+	WSAData data;
+	WORD ver = MAKEWORD(2, 2);
+
+	int wsInit = WSAStartup(ver, &data);
+	// TODO: Inform caller the error that occured
+
+	return wsInit == 0;
+}
+
+// The main processing loop
+void CTcpListener::Run()
+{
+	char buf[MAX_BUFFER_SIZE];
+
+	while (true)
+	{
+		// Create a listening socket
+		SOCKET listening = CreateSocket();
+		if (listening == INVALID_SOCKET)
+		{
+			break;
+		}
+
+		SOCKET client = WaitForConnection(listening);
+		if (client != INVALID_SOCKET)
+		{
+			closesocket(listening);
+			std::cout << "Client connect ok!" << std::endl;
+			int bytesReceived = 0;
+			do
+			{
+				ZeroMemory(buf, MAX_BUFFER_SIZE);
+
+				bytesReceived = recv(client, buf, MAX_BUFFER_SIZE, 0);
+				if (bytesReceived > 0)
+				{
+					if (MessageReceived != NULL)
+					{
+						MessageReceived(this, client, std::string(buf, 0, bytesReceived));
+					}
+				}
+
+			} while (bytesReceived > 0);
+
+			closesocket(client);
+		}
+	}
+}
+
+void CTcpListener::Cleanup()
+{
+	WSACleanup();
+}
+
+// Create a socket
+SOCKET CTcpListener::CreateSocket()
+{
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening != INVALID_SOCKET)
+	{
+		sockaddr_in hint;
+		hint.sin_family = AF_INET;
+		hint.sin_port = htons(m_port);
+		inet_pton(AF_INET, m_ipAddress.c_str(), &hint.sin_addr);
+
+		int bindOk = bind(listening, (sockaddr*)&hint, sizeof(hint));
+		if (bindOk != SOCKET_ERROR)
+		{
+			int listenOk = listen(listening, SOMAXCONN);
+			if (listenOk == SOCKET_ERROR)
+			{
+				return -1;
+			}
+		}
+		else
+		{
+			return -1;
+		}
+	}
+
+	return listening;
+}
+
+// Wait for a connection
+SOCKET CTcpListener::WaitForConnection(SOCKET listening)
+{
+	SOCKET client = accept(listening, NULL, NULL);
+	return client;
+}
+
+
+// Constructor
+CQotd::CQotd(std::string filename)
+{
+	std::ifstream file;
+	file.open(filename);
+	if (file.is_open())
+	{
+		std::string line;
+		std::string running = "";
+
+		while (getline(file, line))
+		{
+			if (line != "%")
+			{
+				running = running + line + "\n";
+			}
+			else
+			{
+				quotes.push_back(running);
+				running = "";
+			}
+		}
+	}
+}
+
+// Get a random quote from the file
+std::string CQotd::GetRandomQuote()
+{
+	int r = rand() % quotes.size();
+	return quotes[r];
+}
+
+
+//#include <iostream>
+//#include <string>
+//
+//#include "TcpListener.h"
+//#include "Qotd.h"
+//
+//using namespace std;
+//
+//void Listener_MessageReceived(CTcpListener* listener, int client, string msg);
+//
+//// This is kinda bad because it's global.
+//CQotd quotes("wisdom.txt");
+//
+//void main()
+//{
+//	CTcpListener server("127.0.0.1", 8082, Listener_MessageReceived);
+//
+//	if (server.Init())
+//	{
+//		server.Run();
+//	}
+//}
+//
+//void Listener_MessageReceived(CTcpListener* listener, int client, string msg)
+//{
+//	if (msg == "QUOTE")
+//	{
+//		listener->Send(client, quotes.GetRandomQuote());
+//	}
+//}
+
 
 TestNetwork::TestNetwork()
 {
--- Projects/TestNetwork.h
+++ Projects/TestNetwork.h
@@ -1,10 +1,79 @@
 #pragma once
 #include <WS2tcpip.h>
+#pragma comment (lib,"ws2_32.lib")
+
+
 
 #include "defines.h"
 #include "IUpdateDraw.h"
 
 
+
+#define MAX_BUFFER_SIZE (49152)
+
+// Forward declaration of class
+class CTcpListener;
+
+// Callback to data received
+typedef void(*MessageRecievedHandler)(CTcpListener* listener, int socketId, std::string msg);
+
+class CTcpListener
+{
+
+public:
+
+	// Constructor
+	CTcpListener(std::string ipAddress, int port, MessageRecievedHandler handler);
+
+	// Destructor
+	~CTcpListener();
+
+	// Send a message to the specified client
+	void Send(int clientSocket, std::string msg);
+
+	// Initialize winsock
+	bool Init();
+
+	// The main processing loop
+	void Run();
+
+	// Clean up after using the service
+	void Cleanup();
+
+private:
+
+	// Create a socket
+	SOCKET CreateSocket();
+
+	// Wait for a connection
+	SOCKET WaitForConnection(SOCKET listening);
+
+	// Address of the server
+	std::string				m_ipAddress;
+
+	// Listening port
+	int						m_port;
+
+	// Message received event handler
+	MessageRecievedHandler	MessageReceived;
+};
+
+
+class CQotd
+{
+public:
+
+	// Constructor
+	CQotd(std::string filename);
+
+	// Get a random quote from the file
+	std::string GetRandomQuote();
+
+private:
+
+	// The quotes
+	std::vector<std::string> quotes;
+};
 
 
 class TestNetwork :public IUpdateDraw
